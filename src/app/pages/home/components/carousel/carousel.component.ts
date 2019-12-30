eee8a11cf85334eb8cf93080e795e03a0abab6ea
Insomnia <282132315@qq.com>
2019-12-30 16:49:44 +0800

    add: banner slide done

--- src/app/pages/home/components/carousel/carousel.component.ts
+++ src/app/pages/home/components/carousel/carousel.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit, TemplateRef, ViewChild, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+  Input,
+  ChangeDetectionStrategy,
+  Output,
+  EventEmitter
+} from "@angular/core";
 
 @Component({
   selector: "app-carousel",
   templateUrl: "./carousel.component.html",
-  styleUrls: ["./carousel.component.less"]
+  styleUrls: ["./carousel.component.less"],
+  // 变更检测策略
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CarouselComponent implements OnInit {
-  @Input() activeIndex: boolean;
+  @Output() changeSlide = new EventEmitter<"pre" | "next">();
+  @Input() activeIndex = 0;
   @ViewChild("dot", { static: true }) dotRef: TemplateRef<any>;
 
   constructor() {}
   ngOnInit() {}
+
+  onChangeSlide(type: "next" | "pre") {
+    this.changeSlide.emit(type);
+  }
 }
