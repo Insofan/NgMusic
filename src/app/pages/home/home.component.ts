ed4ab0a0265d0c82ad74b4af65036edbba29da6a
Insomnia <282132315@qq.com>
2019-12-30 23:42:03 +0800

    add:  tags & personal request

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { HomeService } from "src/app/services/home.service";
 import { Banner } from "src/app/services/data-types/common.types";
-import { NzCarouselComponent } from 'ng-zorro-antd';
+import { NzCarouselComponent } from "ng-zorro-antd";
 
 @Component({
   selector: "app-home",
@@ -12,20 +12,38 @@ export class HomeComponent implements OnInit {
   carouselActiveIndex = 0;
   banners: Banner[];
 
-  @ViewChild(NzCarouselComponent, {static: true}) private nzCarousel: NzCarouselComponent;
+  @ViewChild(NzCarouselComponent, { static: true })
+  private nzCarousel: NzCarouselComponent;
 
   constructor(private homeService: HomeService) {
+    this.getBanners();
+    this.getHotTags();
+    this.getPersonalizedSheetList();
+  }
+
+  private getBanners() {
     this.homeService.getBanners().subscribe(banners => {
       this.banners = banners;
     });
   }
 
+  private getHotTags() {
+    this.homeService.getHotTags().subscribe(tags => {
+      console.log(tags);
+    });
+  }
+  private getPersonalizedSheetList() {
+    this.homeService.getPersonalSheetlist().subscribe(sheets=> {
+      console.log(sheets);
+    });
+  }
+
   ngOnInit() {}
 
-  onBeforeChange({to}) {
+  onBeforeChange({ to }) {
     this.carouselActiveIndex = to;
   }
-  onChangeSlide(type: 'pre' | 'next') {
+  onChangeSlide(type: "pre" | "next") {
     this.nzCarousel[type]();
   }
 }
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Inject } from "@angular/core";
 import { ServicesModule, API_CONFIG } from "./services.module";
 import { Observable, from } from "rxjs";
 import { HttpClient } from "@angular/common/http";
-import { Banner } from "./data-types/common.types";
+import { Banner, HotTag, SongSheet } from "./data-types/common.types";
 import { map } from "rxjs/internal/operators";
 
 @Injectable({
@@ -15,7 +15,27 @@ export class HomeService {
   ) {}
   getBanners(): Observable<Banner[]> {
     return this.http
-      .get(this.uri + 'banner')
+      .get(this.uri + "banner")
       .pipe(map((res: { banners: Banner[] }) => res.banners));
+  }
+
+  getHotTags(): Observable<HotTag[]> {
+    return this.http.get(this.uri + "playlist/hot").pipe(
+      map((res: { tags: HotTag[] }) => {
+        return res.tags
+          .sort((x: HotTag, y: HotTag) => {
+            return x.position - y.position;
+          })
+          .slice(0, 5);
+      })
+    );
+  }
+
+  getPersonalSheetlist(): Observable<SongSheet[]> {
+    return this.http.get(this.uri + "personalized").pipe(
+      map((res: { result: SongSheet[] }) => {
+        return res.result.slice(0, 16);
+      })
+    );
   }
 }
