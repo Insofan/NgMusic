a518a0bd423b7992ce81f1ab15b97836760e2b15
Insomnia <282132315@qq.com>
2020-01-01 09:14:36 +0800

    add: player

--- src/app/share/ui/ui.module.ts
+++ src/app/share/ui/ui.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { SingleSheetComponent } from './single-sheet/single-sheet.component';
 import { PlayCountPipe } from './play-count.pipe';
+import { PlayerModule } from './player/player.module';
 
 @NgModule({
   declarations: [SingleSheetComponent, PlayCountPipe],
   imports: [
+    PlayerModule
   ],
   exports: [
     SingleSheetComponent,
-    PlayCountPipe
+    PlayCountPipe,
+    PlayerModule
   ]
 })
 export class UiModule { }
