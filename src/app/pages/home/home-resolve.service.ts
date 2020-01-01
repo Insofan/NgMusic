09e77397df16953e11c527d0aeb5c1e9c0415287
Insomnia <282132315@qq.com>
2020-01-01 09:14:10 +0800

    add: resolve router

--- src/app/pages/home/home-resolve.service.ts
+++ src/app/pages/home/home-resolve.service.ts
@@ -1,33 +1,33 @@
-import { Injectable } from "@angular/core";
-import { Resolve } from "@angular/router";
-import { Observable, forkJoin } from "rxjs";
-import { first } from "rxjs/internal/operators";
+import { Injectable } from '@angular/core'
+import { Resolve } from '@angular/router'
+import { Observable, forkJoin } from 'rxjs'
+import { first } from 'rxjs/internal/operators'
 import {
-  Banner,
-  HotTag,
-  SongSheet,
-  Singer
-} from "src/app/services/data-types/common.types";
-import { HomeService } from "src/app/services/home.service";
-import { SingerService } from "src/app/services/singer.service";
+    Banner,
+    HotTag,
+    SongSheet,
+    Singer
+} from 'src/app/services/data-types/common.types'
+import { HomeService } from 'src/app/services/home.service'
+import { SingerService } from 'src/app/services/singer.service'
 
-type HomeDataType = [Banner[], HotTag[], SongSheet[], Singer[]];
+type HomeDataType = [Banner[], HotTag[], SongSheet[], Singer[]]
 
 @Injectable({
-  providedIn: "root"
+    providedIn: 'root'
 })
 export class HomeResolveService implements Resolve<HomeDataType> {
-  constructor(
-    private homeServer: HomeService,
-    private singerServer: SingerService
-  ) {}
+    constructor(
+        private homeServer: HomeService,
+        private singerServer: SingerService
+    ) {}
 
-  resolve(): Observable<HomeDataType> {
-    return forkJoin([
-      this.homeServer.getBanners(),
-      this.homeServer.getHotTags(),
-      this.homeServer.getPersonalSheetlist(),
-      this.singerServer.getEnterSinger()
-    ]).pipe(first());
-  }
+    resolve(): Observable<HomeDataType> {
+        return forkJoin([
+            this.homeServer.getBanners(),
+            this.homeServer.getHotTags(),
+            this.homeServer.getPersonalSheetlist(),
+            this.singerServer.getEnterSinger()
+        ]).pipe(first())
+    }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,52 +1,52 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { HomeService } from "src/app/services/home.service";
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { HomeService } from 'src/app/services/home.service'
 import {
-  Banner,
-  HotTag,
-  SongSheet,
-  Singer
-} from "src/app/services/data-types/common.types";
-import { NzCarouselComponent } from "ng-zorro-antd";
-import { SingerService } from "src/app/services/singer.service";
-import { ActivatedRoute } from "@angular/router";
-import { map } from "rxjs/internal/operators";
+    Banner,
+    HotTag,
+    SongSheet,
+    Singer
+} from 'src/app/services/data-types/common.types'
+import { NzCarouselComponent } from 'ng-zorro-antd'
+import { SingerService } from 'src/app/services/singer.service'
+import { ActivatedRoute } from '@angular/router'
+import { map } from 'rxjs/internal/operators'
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.less"]
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-  carouselActiveIndex = 0;
-  banners: Banner[];
-  hotTags: HotTag[];
-  songSheetList: SongSheet[];
-  singers: Singer[];
+    carouselActiveIndex = 0
+    banners: Banner[]
+    hotTags: HotTag[]
+    songSheetList: SongSheet[]
+    singers: Singer[]
 
-  @ViewChild(NzCarouselComponent, { static: true })
-  private nzCarousel: NzCarouselComponent;
+    @ViewChild(NzCarouselComponent, { static: true })
+    private nzCarousel: NzCarouselComponent
 
-  constructor(
-    private homeService: HomeService,
-    private singerService: SingerService,
-    private route: ActivatedRoute
-  ) {
-    this.route.data
-      .pipe(map(res => res.homeDatas))
-      .subscribe(([banners, hotTags, songSheetList, singers]) => {
-        this.banners = banners;
-        this.hotTags = hotTags;
-        this.songSheetList = songSheetList;
-        this.singers = singers;
-      });
-  }
-  ngOnInit() {}
+    constructor(
+        private homeService: HomeService,
+        private singerService: SingerService,
+        private route: ActivatedRoute
+    ) {
+        this.route.data
+            .pipe(map(res => res.homeDatas))
+            .subscribe(([banners, hotTags, songSheetList, singers]) => {
+                this.banners = banners
+                this.hotTags = hotTags
+                this.songSheetList = songSheetList
+                this.singers = singers
+            })
+    }
+    ngOnInit() {}
 
-  onBeforeChange({ to }) {
-    this.carouselActiveIndex = to;
-  }
+    onBeforeChange({ to }) {
+        this.carouselActiveIndex = to
+    }
 
-  onChangeSlide(type: "pre" | "next") {
-    this.nzCarousel[type]();
-  }
+    onChangeSlide(type: 'pre' | 'next') {
+        this.nzCarousel[type]()
+    }
 }
